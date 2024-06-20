2551809234750a10a3486bf1f4052f1f7d5d251a
Zeyad Shaban <zeyadShapan2004@gmail.com>
2024-06-20 21:57:10 +0300

    init

--- Program.cs
+++ Program.cs
@@ -1,149 +1,122 @@
+class Item(string name, int price, int weight) {
+    public string Name { get; set; } = name;
+    public int Price { get; set; } = price;
+    public int Weight { get; set; } = weight;
+}
+
+
 class Program {
-    static char BOT = 'X';
-    static char PLR = 'O';
-    static Dictionary<int, (int, int)> moveMap = new() {
-        {0, (0,0)},
-        {1, (0,1)},
-        {2, (0,2)},
-        {3, (1,0)},
-        {4, (1,1)},
-        {5, (1,2)},
-        {6, (2,0)},
-        {7, (2,1)},
-        {8, (2,2)},
-    };
+    static int MeasureFitness(int[] individual, Item[] items, int capacity) {
+        int value = 0;
+        int weight = 0;
 
+        for (int i = 0; i < individual.Length; ++i) {
+            value += individual[i] * items[i].Price;
+            weight += individual[i] * items[i].Weight;
+            if (weight > capacity) return 0;
+        }
 
-    static bool PlayMove(int pos, char[][] board, char turn) {
-        if (!moveMap.ContainsKey(pos)) return false;
-        (int, int) posIdx = moveMap[pos];
-        if (board[posIdx.Item1][posIdx.Item2] != ' ') return false;
-
-        board[posIdx.Item1][posIdx.Item2] = turn;
-        return true;
+        return value;
     }
 
-    static int CheckWinner(char[][] board) {
-        // Check rows
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
-                return board[i][0] == BOT ? 1 : -1;
-            }
+    static int[][] CreatePopulation(int popSize, int genesSize) {
+        Random random = new(42);
+        int[][] population = new int[popSize][];
+
+        for (int i = 0; i < popSize; ++i) {
+            population[i] = new int[genesSize];
+            for (int j = 0; j < genesSize; ++j)
+                population[i][j] = random.Next(0, 2);
         }
 
-        // Check columns
-        for (int j = 0; j < 3; j++) {
-            if (board[0][j] != ' ' && board[0][j] == board[1][j] && board[1][j] == board[2][j]) {
-                return board[0][j] == BOT ? 1 : -1;
-            }
-        }
-
-        // Check diagonals
-        if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-            return board[0][0] == BOT ? 1 : -1;
-        }
-
-        if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
-            return board[0][2] == BOT ? 1 : -1;
-        }
-
-        // No winner
-        return 0;
+        return population;
     }
 
+    static float[] CreateWheel(int[][] population, Item[] items, int capacity) {
+        // create fitness model
+        int[] fitness = new int[population.Length];
+        float totalFitness = 0;
 
-    static void DrawBoard(char[][] board) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                Console.Write(board[i][j]);
-                if (j < 2) Console.Write("|");
-            }
-            Console.WriteLine();
-            if (i < 2) Console.WriteLine("-----");
-        }
-    }
-
-    static bool IsFull(char[][] board) {
-        for (int i = 0; i < board.Length; ++i)
-            for (int j = 0; j < board[0].Length; ++j)
-                if (board[i][j] == ' ') return false;
-
-        return true;
-    }
-
-    static (int, int) MinMaxSearch(char[][] board, char turn, int depth, int alpha = int.MinValue, int beta = int.MaxValue) {
-        int winner = CheckWinner(board);
-        if (winner != 0 || depth <= 0) return (winner, -1);
-
-        int bestScore = turn == BOT ? int.MinValue : int.MaxValue;
-        int bestMove = -1;
-
-        for (int i = 0; i < 9; ++i) {
-            if (!PlayMove(i, board, turn)) continue;
-
-            (int score, int _) = MinMaxSearch(board, turn == PLR ? BOT : PLR, depth - 1, alpha, beta);
-            board[moveMap[i].Item1][moveMap[i].Item2] = ' ';
-            if (turn == BOT) {
-                alpha = Math.Max(alpha, score);
-                if (beta <= alpha) break;
-
-                if (score > bestScore) {
-                    bestScore = score;
-                    bestMove = i;
-                }
-            }
-            else {
-                beta = Math.Min(beta, score);
-                if (beta <= alpha) break;
-
-                if (score < bestScore) {
-                    bestScore = score;
-                    bestMove = i;
-                }
-            }
+        for (int i = 0; i < population.Length; ++i) {
+            fitness[i] = MeasureFitness(population[i], items, capacity);
+            totalFitness += fitness[i];
         }
 
-        return (bestMove == -1 ? 0 : bestScore, bestMove);
+        // create slices
+        float[] slices = new float[fitness.Length];
+
+        slices[0] = fitness[0] / totalFitness;
+        for (int i = 1; i < fitness.Length; ++i)
+            slices[i] = slices[i - 1] + fitness[i] / totalFitness;
+
+        return slices;
+    }
+
+    static int BinSearchInBetween(float[] slices, float randProbability) {
+        int min = 0;
+        int max = slices.Length;
+        int individualIdx = -1;
+        bool found = false;
+        while (min < max) {
+            individualIdx = (max - min) / 2 + min;
+            if (randProbability <= slices[individualIdx] && (individualIdx == 0 || randProbability > slices[individualIdx - 1])) {
+                found = true;
+                break;
+            };
+
+            if (slices[individualIdx] < randProbability)
+                min = individualIdx;
+            else
+                max = individualIdx;
+        }
+
+        return found ? individualIdx : -1;
+    }
+
+    static int[][] SingePointCrossOver(int[] parentA, int[] parentB, int xoverPoint) {
+        return [[.. parentA[0..xoverPoint], .. parentB[xoverPoint..]], [.. parentB[0..xoverPoint], .. parentA[xoverPoint..]]];
+    }
+    static int[][] UniformCrossover(int[] parentA, int[] parentB) {
+        int[][] children = new int[2][];
+
+        for (int i = 0; i < children.Length; ++i) {
+            children[i] = new int[parentA.Length];
+            for (int gene = 0; gene < parentA.Length; ++gene)
+                children[i][gene] = parentB[gene] == 0 ? parentA[gene] : parentB[gene];
+        }
+
+        return children;
+    }
+
+    static int[] FlipBitMutation(int[] individual) {
+        for (int i = 0; i < individual.Length; ++i)
+            individual[i] ^= 1;
+        return individual;
     }
 
     static void Main(string[] args) {
-        char[][] board = [
-            [' ', ' ', ' '],
-            [' ', ' ', ' '],
-            [' ', ' ', ' '],
-        ];
+        int capacity = 9;
+        int populationSize = 100;
+        Item[] items = [
+            new("Ring", 4,3),
+            new("Gold", 7, 7),
+            new("Crown", 5,4),
+            new("Coin", 1,1),
+            new("Axe", 4, 5),
+            new("Sword", 3,4),
+            new("Ring", 5, 2),
+            new("Cup", 1, 3),
+    ];
 
-        char turn = PLR;
-        int winner = 0;
-        DrawBoard(board);
-        while (winner == 0) {
-            Console.WriteLine("==============================");
-            Console.WriteLine("================================");
-            Console.WriteLine("==================================");
-            Console.WriteLine("====================================");
-            Console.WriteLine("======================================");
-            int pos;
-            if (turn == PLR) {
-                Console.WriteLine("Enter move: ");
-                pos = int.Parse(Console.ReadLine());
-            }
-            else {
-                (int _, pos) = MinMaxSearch(board, turn, 6);
-            }
+        int[][] population = CreatePopulation(populationSize, items.Length);
 
-            bool valid = PlayMove(pos, board, turn);
-            if (!valid) {
-                Console.WriteLine("Invalid position");
-                continue;
-            }
+        float[] slices = CreateWheel(population, items, capacity);
 
-            DrawBoard(board);
+        Random random = new(42);
+        float randProbability = (float)random.NextDouble() * slices[^1];
 
-            winner = CheckWinner(board);
-            if (IsFull(board)) break;
-            turn = turn == PLR ? BOT : PLR;
-        }
+        int individual = BinSearchInBetween(slices, randProbability);
 
-        Console.WriteLine(winner);
+        Console.WriteLine(individual);
     }
 }
